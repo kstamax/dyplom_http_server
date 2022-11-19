d7ab01a358f19e3d498ddb77138a8f85d4f66ab2
kstamax <poberezhnyimax02@gmail.com>
2022-11-20 00:31:07 +0200

    new models added

--- control/api_main/models.py
+++ control/api_main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class DoorLog(models.Model):
+    DOOR_STATES = (
+        ("opened","opened"),
+        ("closed","closed")
+    )
+    log_date = models.DateTimeField()
+    door_state = models.CharField(max_length=6, choices=DOOR_STATES)
+
+class DeviceLog(models.Model):
+    MESSAGE_TYPES = (
+        ("error", "error"),
+        ("info", "info")
+    )
+    log_date = models.DateTimeField()
+    message_type = models.CharField(max_length=10, choices=MESSAGE_TYPES)
+    message_body = models.TextField()
