22c4bd92abe1cd81e1edccaa05a3c5c6f12695e7
kstamax <poberezhnyimax02@gmail.com>
2022-11-28 17:14:29 +0200

    collect static files

---
+++ control/staticfiles/main/scripts/devicelog.js
@@ -0,0 +1,52 @@
+const BASE_API_URL = `api/devicelogs`
+    buildTable();
+    function buildTable(){
+        var lst = document.getElementById("itemsList");
+        lst.innerHTML = '';
+        var url = BASE_API_URL;
+        fetch(url)
+        .then((resp)=>resp.json())
+        .then(function(data){
+            var list = data;
+            for (var i in list){
+                text_color = ''
+                if(list[i]['message_type'] === 'INFO'){
+                    text_color='text-warning'
+                }
+                else{
+                    text_color='text-danger'
+                } 
+                var item = `
+                <li class="list-group-item ${text_color}">${list[i]['message_type']}    ${list[i]['log_date']}    ${list[i]['message_body']}</li>
+                `;
+                lst.innerHTML += item;
+            }
+        })
+    }
+    setInterval(function(){ 
+        liveUpdate()   
+    }, 2000);
+    function liveUpdate(){
+        var lst = document.getElementById("itemsList");
+        var url = BASE_API_URL;
+        fetch(url)
+        .then((resp)=>resp.json())
+        .then(function(data){
+            var list = data;
+            for (var i in list){
+                text_color = ''
+                if(list[i]['message_type'] === 'INFO'){
+                    text_color='text-warning'
+                }
+                else{
+                    text_color='text-danger'
+                } 
+                var item = `
+                <li class="list-group-item ${text_color}">${list[i]['message_type']}    ${list[i]['log_date']}    ${list[i]['message_body']}</li>
+                `;
+                if(!lst.innerHTML.includes(item)){
+                    lst.innerHTML += item;
+                }
+            }
+        })
+    }
