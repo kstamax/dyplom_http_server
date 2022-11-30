01f38c2dba997f15fb7f5e934c0e3630007ad95e
kstamax <poberezhnyimax02@gmail.com>
2022-11-30 02:29:17 +0200

    added allowed host

--- control/control/settings.py
+++ control/control/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#lu64ej&k&3gie(xh_b^6ds%^^bjeq(ou6n#5t$hn8_*2c-c5)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.104']
 
 
 # Application definition
